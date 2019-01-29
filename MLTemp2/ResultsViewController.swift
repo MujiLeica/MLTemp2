4f6d973e535c5b1f40b52c4bd44626d60f9429b1
MujiLeica <chongl4@student.unimelb.edu.au>
2019-01-30 02:40:59 +1100

    Before Changing label to button

---
+++ MLTemp2/ResultsViewController.swift
@@ -0,0 +1,23 @@
+//
+//  ResultsViewController.swift
+//  MLTemp2
+//
+//  Created by CHONG LIU on 30/1/19.
+//  Copyright © 2019 CHONG LIU. All rights reserved.
+//
+
+import UIKit
+
+class ResultsViewController: UIViewController {
+    
+    
+    @IBOutlet weak var firstResultLabel: UILabel!
+    
+    @IBOutlet weak var secondResultLabel: UILabel!
+    
+    @IBOutlet weak var thirdResultLabel: UILabel!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
